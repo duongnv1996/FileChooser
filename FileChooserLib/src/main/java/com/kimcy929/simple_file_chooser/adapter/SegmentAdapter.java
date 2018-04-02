c557ee0d7ecc3ca04e5b462faf981edfcc074097
kimcy929 <kimcy92@gmail.com>
2018-04-02 15:06:45 +0700

    Cleanup, handle the root path, change click item event

--- FileChooserLib/src/main/java/com/kimcy929/simple_file_chooser/adapter/SegmentAdapter.java
+++ FileChooserLib/src/main/java/com/kimcy929/simple_file_chooser/adapter/SegmentAdapter.java
@@ -16,8 +16,6 @@ import com.kimcy929.simple_file_chooser.R;
 import java.io.File;
 import java.util.List;
 
-import timber.log.Timber;
-
 public class SegmentAdapter extends RecyclerView.Adapter<SegmentAdapter.ViewHolder> {
     private List<String> segmentList;
 
@@ -45,22 +43,6 @@ public class SegmentAdapter extends RecyclerView.Adapter<SegmentAdapter.ViewHold
         segmentList = newData;
         notifyDataSetChanged();
         onItemClickListener.scrollToLastSegment();
-
-        /*if(segmentList == null) {
-           if (newData != null) {
-               segmentList = newData;
-               notifyDataSetChanged();
-               onItemClickListener.scrollToLastSegment();
-           }
-        } else {
-            if (newData == null) {
-                segmentList.clear();
-                notifyDataSetChanged();
-            } else {
-                diffTask = new DiffTask(this);
-                diffTask.execute(newData);
-            }
-        }*/
     }
 
     @SuppressWarnings("WeakerAccess")
@@ -75,27 +57,21 @@ public class SegmentAdapter extends RecyclerView.Adapter<SegmentAdapter.ViewHold
             txtSegment = itemView.findViewById(R.id.txtSegment);
             imageArrowIcon = itemView.findViewById(R.id.imageArrowIcon);
 
-            itemView.setOnClickListener(new View.OnClickListener() {
+            txtSegment.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     int adapterPosition = getAdapterPosition();
                     // rebuild path
                     StringBuilder pathBuilder = new StringBuilder();
-                    for (int i = 0; i <= adapterPosition; i++) {
-                        pathBuilder.append(File.separator).append(segmentList.get(i));
+                    if (adapterPosition == 0) {
+                        pathBuilder.append(File.separator);
+                    } else {
+                        for (int i = 1; i <= adapterPosition; i++) {
+                            pathBuilder.append(File.separator).append(segmentList.get(i));
+                        }
                     }
                     onItemClickListener.pathSegmentClick(pathBuilder.toString());
                 }
-
-                /*private void removeRangeItem(int adapterPosition) {
-                    int j = 0;
-                    int size = segmentList.size();
-                    for (int i = adapterPosition + 1; i < size; ++i) {
-                        segmentList.remove(i - j);
-                        ++j;
-                    }
-                    notifyItemRangeRemoved(adapterPosition + 1, size);
-                }*/
             });
         }
 
@@ -106,8 +82,6 @@ public class SegmentAdapter extends RecyclerView.Adapter<SegmentAdapter.ViewHold
             } else {
                 txtSegment.setText(R.string.root_folder_name);
             }
-
-            Timber.d("position -> %s, itemCount %s", position, getItemCount());
 
             if (position < getItemCount() - 1) {
                 imageArrowIcon.setVisibility(View.VISIBLE);
@@ -131,88 +105,4 @@ public class SegmentAdapter extends RecyclerView.Adapter<SegmentAdapter.ViewHold
         void pathSegmentClick(String path);
         void scrollToLastSegment();
     }
-
-    /*
-    private DiffTask diffTask;
-
-    public void cancelDiffTask() {
-        if (diffTask != null && !diffTask.isCancelled()) {
-            diffTask.cancel(true);
-        }
-    }
-
-    private static class SegmentDiff extends DiffUtil.Callback {
-
-        private List<String> oldSegment;
-        private List<String> newSegment;
-
-        @SuppressWarnings("WeakerAccess")
-        public SegmentDiff(List<String> oldSegment, List<String> newSegment) {
-            this.oldSegment = oldSegment;
-            this.newSegment = newSegment;
-        }
-
-        @Override
-        public int getOldListSize() {
-            return oldSegment != null ? oldSegment.size() : 0;
-        }
-
-        @Override
-        public int getNewListSize() {
-            return newSegment != null ? newSegment.size() : 0;
-        }
-
-        @Override
-        public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-            return TextUtils.equals(oldSegment.get(oldItemPosition), newSegment.get(newItemPosition));
-        }
-
-        @Override
-        public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-            return false;
-        }
-
-        @Nullable
-        @Override
-        public Object getChangePayload(int oldItemPosition, int newItemPosition) {
-            Timber.d("oldItemPosition %s, newItemPosition %s", oldItemPosition, newItemPosition);
-            Timber.d("getOldSize %s, getNewSize %s", getOldListSize(), getNewListSize());
-            return super.getChangePayload(oldItemPosition, newItemPosition);
-        }
-    }
-
-    private static class DiffTask extends AsyncTask<List<String>, Void, DiffUtil.DiffResult> {
-        private WeakReference<SegmentAdapter> segmentAdapterWeakReference;
-        private List<String> newData;
-
-        @SuppressWarnings("WeakerAccess")
-        public DiffTask(SegmentAdapter segmentAdapter) {
-            segmentAdapterWeakReference = new WeakReference<>(segmentAdapter);
-        }
-
-        @SafeVarargs
-        @Override
-        protected final DiffUtil.DiffResult doInBackground(List<String>... lists) {
-            if (segmentAdapterWeakReference.get() != null) {
-                newData = lists[0];
-                return DiffUtil.calculateDiff(new SegmentDiff(segmentAdapterWeakReference.get().segmentList, newData), false);
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(DiffUtil.DiffResult diffResult) {
-            super.onPostExecute(diffResult);
-            SegmentAdapter segmentAdapter = segmentAdapterWeakReference.get();
-            if (segmentAdapter != null && diffResult != null) {
-
-                segmentAdapter.segmentList.clear();
-                segmentAdapter.segmentList.addAll(newData);
-
-                diffResult.dispatchUpdatesTo(segmentAdapter);
-
-                segmentAdapter.onItemClickListener.scrollToLastSegment();
-            }
-        }
-    }*/
 }
